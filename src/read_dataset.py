22bf1d21a7f8f3e3d359571cf71cc7de1efce8d3
changhao <changhao@bu.edu>
2020-02-21 16:53:16 -0500

    123

--- src/read_dataset.py
+++ src/read_dataset.py
@@ -11,31 +11,31 @@ with open('cases_appeals.json', 'r') as f:
 print(data[0])
 
 
-# def loop(data):
-#     for key in data.keys():
-#         for ele in data[key]:
-#             for word in Words:
-#                 # if(ele.find(Head_Word) and (not ele.find("civil"))):
-#                 if (Head_Word.lower() in ele.lower()):
-#                     if (word.lower() in ele.lower()):
-#                         return True
-#     return False
+def loop(data):
+    for key in data.keys():
+        for ele in data[key]:
+            for word in Words:
+                # if(ele.find(Head_Word) and (not ele.find("civil"))):
+                if (Head_Word.lower() in ele.lower()):
+                    if (word.lower() in ele.lower()):
+                        return True
+    return False
 
-# A = []
-# for i in range(len(data)):
-#     A.append(loop(data[i]))
-# n = 0
-# List = []
-# for i in range(len(A)):
-#     List.append(data[i])
-#     if(A[i] == True):
-#         n += 1
+A = []
+for i in range(len(data)):
+    A.append(loop(data[i]))
+n = 0
+List = []
+for i in range(len(A)):
+    List.append(data[i])
+    if(A[i] == True):
+        n += 1
 
 
-# print("Total number of cases: ")
-# print(len(A))
-# print("Positive: ")
+print("Total number of cases: ")
+#print(len(A))
+print("Positive: ")
 # print(n)
-#print(List)
-# with open("cases_appeals_result.json","w") as f:
-#      json.dump(List, f)
+# print(List)
+with open("cases_appeals_result.json","w") as f:
+     json.dump(List, f)
