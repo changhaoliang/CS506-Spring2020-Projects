286b40b6f0d768b0d626a85ef979011dc4729f31
Chen Xin <hsinchen1996@gmail.com>
2020-02-24 20:51:26 -0500

    result 97

--- src/read_dataset.py
+++ src/read_dataset.py
@@ -1,41 +1,49 @@
 import json
-
+import re
+import pprint
 dict = {}
 
 Head_Word = "Criminal"
-Words = ["CI", "confidental informat", "informat"]
+Words = [ "confidential informant", "informant"]
+A=set([])
+B=set([])
+C=[]
+
+filteredCases = []
+with open('cases.json', 'r') as f:
+    cases = json.load(f)
 
 with open('cases_appeals.json', 'r') as f:
-    data = json.load(f)
+    cases_appeals = json.load(f)
 
-print(data[0])
+for i, case in enumerate(cases):
+    for key in case:
+        if key == "headnote" :
+            for headnote in case[key]:
+                if "Criminal" in headnote:
+                    filteredCases.append(cases[i])
+
+for i, case in enumerate(cases_appeals):
+    for key in case:
+        if key == "headnote" :
+            for headnote in case[key]:
+                if "Criminal" in headnote:
+                    filteredCases.append(cases_appeals[i])
+
+for i, case in enumerate(filteredCases):
+    for key in case:
+        for text in case[key]:
+            words = text.split(' ')
+            for word in words:
+                pattern = re.compile(r'informant',re.IGNORECASE)
+                result = pattern.findall(word)
+                if result!=[]:
+                    A.add(i)
+                else:
+                    pattern = re.compile(r'CI')
+                    result = pattern.findall(word)
+                    if result!=[]:
+                        A.add(i)
 
 
-def loop(data):
-    for key in data.keys():
-        for ele in data[key]:
-            for word in Words:
-                # if(ele.find(Head_Word) and (not ele.find("civil"))):
-                if (Head_Word.lower() in ele.lower()):
-                    if (word.lower() in ele.lower()):
-                        return True
-    return False
-
-A = []
-for i in range(len(data)):
-    A.append(loop(data[i]))
-n = 0
-List = []
-for i in range(len(A)):
-    List.append(data[i])
-    if(A[i] == True):
-        n += 1
-
-
-print("Total number of cases: ")
-#print(len(A))
-print("Positive: ")
-# print(n)
-# print(List)
-with open("cases_appeals_result.json","w") as f:
-     json.dump(List, f)
+print(len(A))
