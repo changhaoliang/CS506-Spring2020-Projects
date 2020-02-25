8a157f1a2af9798de65cfc911f8a80d27853d29c
changhao <changhao@bu.edu>
2020-02-24 20:20:23 -0500

    add comments

--- src/read_dataset.py
+++ src/read_dataset.py
@@ -6,32 +6,37 @@ Head_Word = "Criminal"
 
 key_words = [ "confidential informant", "informant"]
 
-res = set()
+res = set() # avoid duplicate
 
 with open('cases.json', 'r') as f:
     data = json.load(f)
 
 for i, case in enumerate(data):
+
     for key in case:
         value = case[key]
 
         for text in value:
+            # replace punctuation with space
             for c in string.punctuation:  
                 text = text.replace(c, ' ')
-    
+            
+            # split text with space
             words = text.split(' ')
             
+
             for w in words:
+                # search keyword
                 pattern = re.compile(r'informant',re.IGNORECASE)
-                
                 result = pattern.findall(w)
 
-                if result!=[]:
+                if result != []:
                     res.add(i)
+                    
                 else:
                     pattern = re.compile(r'CI')
                     result = pattern.findall(w)
-                    if result!=[]:
+                    if result != []:
                         res.add(i)
 
 print(len(res))
