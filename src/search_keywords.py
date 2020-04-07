be5040beef905d9ede17c9942f5ef9f3d3b9c5ef
wqtwjt1996 <1061687050@qq.com>
2020-04-06 23:42:18 -0400

    Qitong Wang added the final result analysis, version 1.

--- src/search_keywords.py
+++ src/search_keywords.py
@@ -1,8 +1,88 @@
+#-*- coding: utf-8 -*-
 import os
 import csv
 import json
-from pprint import pprint
+import matplotlib.pyplot as plt
+plt.rcParams['font.sans-serif']=['SimHei']
 
+
+def result_visualization(num_affirmed, num_part, num_reversed, num_remanded, num_nonremanded,
+                         num_affirmed_noKeywords, num_part_noKeywords, num_reversed_noKeywords,
+                         num_remanded_noKeywords, num_nonremanded_noKeywords):
+    """
+        :param: all kinds of statistics numbers.
+
+        final result visualization using four pie graphs.
+        edited by Qitong Wang; Apr 6th, 2020.
+    """
+    # 1
+    plt.figure(figsize=(8, 6))
+    labels = [u'affirmed', u'partly affirmed', u'reversed']
+    sizes = [num_affirmed, num_part, num_reversed]
+    colors = ['red', 'lightskyblue', 'yellow']
+    explode = (0, 0, 0)
+    patches, text1, text2 = plt.pie(sizes,
+                                    explode=explode,
+                                    labels=labels,
+                                    colors=colors,
+                                    autopct='%3.2f%%',
+                                    shadow=False,
+                                    startangle=90,
+                                    pctdistance=0.6)
+    plt.title('Statistics of criminal cases containing keywords: affirmation')
+    plt.axis('equal')
+    plt.savefig('pie_pic/KW_affirm.jpg')
+    # 2
+    plt.figure(figsize=(8, 6))
+    labels = [u'affirmed', u'partly affirmed', u'reversed']
+    sizes = [num_affirmed_noKeywords, num_part_noKeywords, num_reversed_noKeywords]
+    colors = ['red', 'lightskyblue', 'yellow']
+    explode = (0, 0, 0)
+    patches, text1, text2 = plt.pie(sizes,
+                                    explode=explode,
+                                    labels=labels,
+                                    colors=colors,
+                                    autopct='%3.2f%%',
+                                    shadow=False,
+                                    startangle=90,
+                                    pctdistance=0.6)
+    plt.title('Statistics of criminal cases not containing keywords: affirmation')
+    plt.axis('equal')
+    plt.savefig('pie_pic/NKW_affirm.jpg')
+    # 3
+    plt.figure(figsize=(8, 6))
+    labels = [u'remanded', u'not remanded']
+    sizes = [num_remanded, num_nonremanded]
+    colors = ['yellowgreen', 'cadetblue']
+    explode = (0, 0)
+    patches, text1, text2 = plt.pie(sizes,
+                                    explode=explode,
+                                    labels=labels,
+                                    colors=colors,
+                                    autopct='%3.2f%%',
+                                    shadow=False,
+                                    startangle=90,
+                                    pctdistance=0.6)
+    plt.title('Statistics of criminal cases containing keywords: remand')
+    plt.axis('equal')
+    plt.savefig('pie_pic/KW_remand.jpg')
+    # 4
+    plt.figure(figsize=(8, 6))
+    labels = [u'remanded', u'not remanded']
+    sizes = [num_remanded_noKeywords, num_nonremanded_noKeywords]
+    colors = ['yellowgreen', 'cadetblue']
+    explode = (0, 0)
+    patches, text1, text2 = plt.pie(sizes,
+                                    explode=explode,
+                                    labels=labels,
+                                    colors=colors,
+                                    autopct='%3.2f%%',
+                                    shadow=False,
+                                    startangle=90,
+                                    pctdistance=0.6)
+    plt.title('Statistics of criminal not cases containing keywords: remand')
+    plt.axis('equal')
+    plt.savefig('pie_pic/NKW_remand.jpg')
 
 def search_keywords(state, keywords):
     """
@@ -20,6 +100,20 @@ def search_keywords(state, keywords):
 
     case_pool = set([])
     cases_keywords = []
+
+    """counting the numbers"""
+    """edited by Qitong Wang; Apr 6th, 2020."""
+    num_affirmed = 0
+    num_part = 0
+    num_reversed = 0
+    num_remanded = 0
+    num_nonremanded = 0
+
+    num_affirmed_N = 0
+    num_part_N = 0
+    num_reversed_N = 0
+    num_remanded_N = 0
+    num_nonremanded_N = 0
     for i, case in enumerate(criminal_cases):
         for key in case:
             for text in case[key]:
@@ -28,15 +122,61 @@ def search_keywords(state, keywords):
                     for keyword in keywords:
                         if keyword in word.lower():
                             if i not in case_pool:
+                                """calculation of the classification labels"""
+                                """edited by Qitong Wang; Apr 6th, 2020."""
+                                if ('affirmed' in case['decision'].lower() and 'part' not in case['decision'].lower()):
+                                    num_affirmed += 1
+                                if ('part' in case['decision'].lower()):
+                                    num_part += 1
+                                if ('reversed' in case['decision'].lower() and 'part' not in case['decision'].lower()):
+                                    num_reversed += 1
+                                if ('remanded' in case['decision'].lower()):
+                                    num_remanded += 1
+                                else:
+                                    num_nonremanded += 1
                                 cases_keywords.append(case)
                             case_pool.add(i)
-                    if "CI" in word:
-                        if i not in case_pool:
-                            cases_keywords.append(case)
-                        #print(word)
-                        case_pool.add(i)
+    """calculating the total number of cases, so if we want to get cases without the number of keywords, we could minus the number of cases containing keywords"""
+    """edited by Qitong Wang; Apr 6th, 2020."""
+    for case in criminal_cases:
+        if ('affirmed' in case['decision'].lower() and 'part' not in case['decision'].lower()):
+            num_affirmed_N += 1
+        if ('part' in case['decision'].lower()):
+            num_part_N += 1
+        if ('reversed' in case['decision'].lower() and 'part' not in case['decision'].lower()):
+            num_reversed_N += 1
+        if ('remanded' in case['decision'].lower()):
+            num_remanded_N += 1
+        else:
+            num_nonremanded_N += 1
 
-    print(str(len(case_pool))+" cases contain keywords")
+    print(str(len(case_pool)) + " cases contain keywords")
+    print(str(len(criminal_cases) - len(case_pool)) + " cases do not contain keywords")
+
+    """show the results"""
+    """edited by Qitong Wang; Apr 6th, 2020."""
+    print("******************************************************")
+    print('In cases containing keywords:')
+    print('Affirmed case number: ' + str(num_affirmed))
+    print('Partly affirmed case number: ' + str(num_part))
+    print('Reversed case number: ' + str(num_reversed))
+    print("------------------------------------------------------")
+    print('Remanded case number: ' + str(num_remanded))
+    print('Nonremanded case number: ' + str(num_nonremanded))
+    print("******************************************************")
+    print('In cases not containing keywords:')
+    print('Affirmed case number: ' + str(num_affirmed_N - num_affirmed))
+    print('Partly affirmed case number: ' + str(num_part_N - num_part))
+    print('Reversed case number: ' + str(num_reversed_N - num_reversed))
+    print("------------------------------------------------------")
+    print('Remanded case number: ' + str(num_remanded_N - num_remanded))
+    print('Nonremanded case number: ' + str(num_nonremanded_N - num_nonremanded))
+    print("******************************************************")
+
+    result_visualization(num_affirmed, num_part, num_reversed, num_remanded, num_nonremanded,
+                         num_affirmed_N - num_affirmed, num_part_N - num_part, num_reversed_N - num_reversed,
+                         num_remanded_N - num_remanded, num_nonremanded_N - num_nonremanded)
+
     return cases_keywords
 
 
@@ -95,12 +235,14 @@ if __name__ == "__main__":
 
     # enter "ma", "nh" or "ri"
     m_state = "nh"
-    m_keywords = ["informant"]
+    m_keywords = ["informant", "CI"]
+    # m_result = "affirmed"
 
     result = search_keywords(m_state, m_keywords)
 
     # save to ../data
     save_path = os.path.dirname(os.path.dirname(os.path.abspath(__file__))) + "/result/cases_" + m_state + "_result"
+    # save_path = os.path.dirname(os.path.dirname(os.path.abspath(__file__))) + "/result/cases_" + m_state + "_" + m_result + "_result"
     # save as json or csv
     save_result(save_path, "json", result)
 
